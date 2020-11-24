e402ee4314b33df6070fb651dfbd7b17b79b9afc
Gustavo Brito <britopereiragustavo@gmail.com>
2020-11-24 15:46:29 -0300

    creating sub folders in structure

--- site_downloader/downloader/site_structure.py
+++ site_downloader/downloader/site_structure.py
@@ -3,8 +3,10 @@ import os
 
 def create_structure(response):
     pattern = response.url.split('/')
+    folder = ''.join([str(elem) + '/' for elem in pattern[2:-1]])
     local_dir = os.path.dirname(os.path.abspath(__file__))
     try:
-        os.makedirs(f'{local_dir}/../../{pattern[2]}')
+        os.makedirs(f'{local_dir}/../../{folder}')
     except FileExistsError:
         pass
+g
