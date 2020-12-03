b26f956608dd0beeda500956ed7fe4da11495374
Gustavo Brito <britopereiragustavo@gmail.com>
2020-12-03 11:05:55 -0300

    refact

--- site_downloader/downloader/statics.py
+++ site_downloader/downloader/statics.py
@@ -1,15 +1,14 @@
-import os
 from urllib.request import urlretrieve
 from bs4 import BeautifulSoup
 
+from site_downloader.downloader.utils import static_path
+
 
 def download_statics(response):
-    splited_url = response.url.split('/')
     bs = BeautifulSoup(response.text, 'html.parser')
-    path = os.path.dirname(os.path.abspath(__file__)) + '/../../' + splited_url[2] + '/static_files/img/' + ''.join(f + '/' for f in splited_url[3:-1])
 
     for img in bs.find_all('img'):
         if 'src' in img.attrs:
             img_src = img.attrs['src']
-            img_path = path + img.attrs['src'].split('/')[-1]
+            img_path = static_path(response, 'img') + img.attrs['src'].split('/')[-1]
             urlretrieve(img_src, img_path)
--- site_downloader/downloader/utils.py
+++ site_downloader/downloader/utils.py
@@ -1,3 +1,5 @@
+import os
+
 def get_file_location(response):
     splited_url = response.url.split('/')
 
@@ -7,3 +9,10 @@ def get_file_location(response):
 
     folder = ''.join([str(elem) + '/' for elem in splited_url[2:-1]])
     return f'{folder}/{name}'
+
+
+def static_path(response, static_type):
+    splited_url = response.url.split('/')
+    root_path = os.path.dirname(os.path.abspath(__file__)) + '/../../' + splited_url[2]
+    path = ''.join(f + '/' for f in splited_url[3:-1])
+    return f'{root_path}/static_files/{static_type}/{path}'
